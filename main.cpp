d5b04d7891ff6e4cd857368a625437f94aa1cde2
helen-black <olenablack@gmail.com>
2018-10-16 14:33:53 +0300

    Added vector of numbers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
+#include <vector>
 int main()
 {
+    std::vector<int> numbers(5);
+
+    for (auto &elem : numbers)
+        elem = 3;
 
     std::cout << "Hello!!!" << std::endl;
     return 0;
